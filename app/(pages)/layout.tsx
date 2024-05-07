914655a1870e52e6b500cba270240166622b0788
tjfaith <tjbenbiz@gmail.com>
2024-05-07 23:56:01 +0100

    updated

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect } from "react";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-
+import "react-multi-carousel/lib/styles.css";
 
 const PageLayout = ({ children }: { children: React.ReactNode }) => {
   useEffect(() => {
--- app/components/homePage/Partners.tsx
+++ app/components/homePage/Partners.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Image from "next/image";
+import Carousel from "react-multi-carousel";
+
 import {
   AccessBankLogo,
   FCMBLogo,
@@ -10,15 +12,40 @@ import {
   WemaBankLogo,
   UBALogo,
   Acc1,
-  Acc2
+  Acc2,
 } from "@/app/assets";
 
 const Partners = () => {
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 10,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 8,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 6,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 4,
+    },
+  };
+
   return (
     <section className="w-full text-center mt-20 px-28 flex flex-col gap-10 ">
       <div className="text-gray-700 font-semibold">Meet our partners</div>
-      <div className="flex items-center w-full justify-between" data-aos="fade-up"
-     data-aos-anchor-placement="top-bottom">
+      <Carousel
+        responsive={responsive}
+        autoPlay
+        swipeable
+        draggable
+        infinite
+        arrows={false}
+      >
         <Image src={AccessBankLogo} alt="Access Bank" />
         <Image src={WemaBankLogo} alt="Sterling Bank" />
         <Image src={ZenithBankLogo} alt="Zenith Bank" />
@@ -27,10 +54,20 @@ const Partners = () => {
         <Image src={UBALogo} alt="UBA Bank" />
         <Image src={SterlingBankLogo} alt="Sterling Bank" />
         <Image src={HeritageBankLogo} alt="Heritage Bank" />
-      </div>
+      </Carousel>
       <div className="flex items-center justify-center space-x-7">
-      <Image src={Acc1} alt="Accredited 1" data-aos="zoom-in-up" data-aos-delay="300"/>
-      <Image src={Acc2} alt="Accredited 2" data-aos="zoom-in-up" data-aos-delay="400"/>
+        <Image
+          src={Acc1}
+          alt="Accredited 1"
+          data-aos="zoom-in-up"
+          data-aos-delay="300"
+        />
+        <Image
+          src={Acc2}
+          alt="Accredited 2"
+          data-aos="zoom-in-up"
+          data-aos-delay="400"
+        />
       </div>
     </section>
   );
